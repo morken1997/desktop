6f47a791032107fbc195c74eebd562bef8a520fd
Morken Shawn <30389644+morken1997@users.noreply.github.com>
2021-06-19 00:00:52 +0800

    update

---
+++ js/news.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+    var devNews = document.getElementsByClassName("devNews");
+    console.log(devNews.length);
+    $.ajax({
+        type: "GET",
+        url: "https://dev.to/api/listings",
+        dataType: "json",
+        success: function (response) {
+            var devNews = document.getElementsByClassName("devNews");
+            console.log(devNews.length);
+
+            for (let i = 0; i <= 10; i++) {
+                devNews[i].innerHTML = JSON.stringify(response[i].title)
+                // console.log(JSON.stringify(response[i]));
+
+            }
+        }
+    });
+});
+
+
+// window.onload = function news() {
+//     var devNews = document.getElementsByClassName("devNews");
+//     console.log(devNews.length);
+//     $.ajax({
+//         type: "GET",
+//         url: "https://dev.to/api/listings",
+//         dataType: "json",
+//         success: function (response) {
+//             var devNews = document.getElementsByClassName("devNews");
+//             console.log(devNews.length);
+
+//             for (let i = 0; i <= 10; i++) {
+//                 devNews[i].innerHTML = '<p>' + i + '</p';
+//                 // console.log(JSON.stringify(response[i]));
+
+//             }
+//         }
+//     });
+// }
+
+
+// window.onload = function loadXMLDoc() {
+//     var xmlhttp;
+//     if (window.XMLHttpRequest) {
+//         //  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+//         xmlhttp = new XMLHttpRequest();
+//     }
+//     else {
+//         // IE6, IE5 浏览器执行代码
+//         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+//     }
+//     xmlhttp.onreadystatechange = function () {
+//         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+//             document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+//             console.log(xmlhttp.responseText);
+//             for (let index = 0; index < array.length; index++) {
+//                 const element = array[index];
+
+//             }
+//         }
+//     }
+//     xmlhttp.open("GET", "https://dev.to/api/listings", true);
+//     xmlhttp.send();
+// }
